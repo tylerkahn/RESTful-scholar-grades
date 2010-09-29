ba327c21f6ff4452fe39bd30f09960ee84532b1d
Tyler Kahn <tkahn6@gmail.com>
2010-09-29 01:15:13 -0400

    clearing cookies requires newer package of zope.testbrowser

--- grades.py
+++ grades.py
@@ -25,7 +25,8 @@ def to_json(x):
 ###############################################
 
 def logInToScholar(pid, password):
-	browser.cookies.clearAll()
+    # requires newer zope.testbrowser package 
+    # browser.cookies.clearAll()
 	browser.open("https://scholar.vt.edu")
 	form = browser.getForm(id='loginForm')
 	form.getControl(name="eid").value = pid
